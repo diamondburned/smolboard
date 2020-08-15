c365feb776b6d2427649cd4b07c531cf285230c2
diamondburned <datutbrus@gmail.com>
2020-08-14 20:34:17 -0700

    Fixed wrong thumbnail cache path

--- server/http/upload/imgsrv/thumbcache/thumbcache.go
+++ server/http/upload/imgsrv/thumbcache/thumbcache.go
@@ -4,6 +4,7 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/c2h5oh/datasize"
 	"github.com/peterbourgon/diskv"
@@ -12,7 +13,7 @@ import (
 var thumbCache = diskv.New(diskv.Options{
 	BasePath: filepath.Join(os.TempDir(), "smolboard-thumbs"),
 	Transform: func(s string) []string {
-		return []string{url.PathEscape(s)}
+		return []string{url.PathEscape(strings.TrimRight(s, "/"))}
 	},
 	// 4MB cache in memory strictly.
 	CacheSizeMax: uint64(4 * datasize.MB),
